0bea0083a4bc322989404932d6a6b09347a12956
Katie Maddox <kmaddox2122@gmail.com>
2022-10-13 10:51:24 -0500

    added km API keys

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = {
-    MY_YOUTUBE_API: "",
-    MY_MOVIE_API : "",
-    MY_GIPHY_API : "wvV8XbpunUwtfHCUuAUptsJJmdb4yn01"
-  }
+  MY_YOUTUBE_API: "AIzaSyClJT_YIc2Vh0Aezue5FXzZ4X0K32N1SRo",
+  MY_MOVIE_API : "c0bd93f36amshfaeaae0b910c85bp18a115jsn3771b8f37325",
+  MY_GIPHY_API : "wvV8XbpunUwtfHCUuAUptsJJmdb4yn01",
+}
