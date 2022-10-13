593ce6bcbbad951824c971b77dd44404d7579f74
Sarah Thoorens <[s.thoorens@gmail.com]>
2022-10-13 12:27:54 -0500

    frontpage upates

--- Asset/js/frontpage-script.js
+++ Asset/js/frontpage-script.js
@@ -44,7 +44,7 @@ function getMovieApi(movieInput) {
 	const options = {
 		method: 'GET',
 		headers: {
-			'X-RapidAPI-Key': '0d66aff19dmsh524092da5aa051fp10c216jsn8b26997c98ec',
+			'X-RapidAPI-Key': config.MY_MOVIE_API,
 			'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
 		}
 	};
@@ -79,48 +79,64 @@ function init() {
 	else {  getMovieApi(movieInput);
 	}
 
-	let {movieData, results } = searchHistory;
+	
   // render movie posters of stored movies to the DOM
 	 // Clear movie snippets
-	 
 	 moviePoster.innerHTML = "";
 	 
 	 let lastSearch = searchHistory[searchHistory.length - 1]
-	 for (var i = 0; i < 3; i++) {  
-		
-	 const searchHistoryContent = `
-	  <div class="moviePoster-1 cell-small-4">
-		  <div class = "card">
-			  <div class = "card-section">
-				  <div class="centered-text">
-					  <h4>${lastSearch.movieData.results[0].title}</h4>
-				  </div>	
-			  <a href="<!--link to modal-->"><img src="${lastSearch.movieData.results[0].image.url}" alt="${lastSearch.movieData.results[0].title} poster style="width:100%""></a>
-		  </div>
-			  </div>
-	  </div>
-	  <div class="moviePoster-2 cell-small-4">
-		  <div class = "card">
-			  <div class = "card-section">
-				  <div class="centered-text">
-					  <h4>${lastSearch.movieData.results[1].title}</h4>
-				  </div>	
-			  <a href="<!--link to modal-->"><img src="${lastSearch.movieData.results[1].image.url}" alt="${lastSearch.movieData.results[1].title} poster style="width:100%""></a>
-		  </div>
-			  </div>
-	  </div>
-	  <div class="moviePoster-3 cell-small-4">
-		  <div class = "card">
-			  <div class = "card-section">
-				  <div class="centered-text">
-					  <h4>${lastSearch.movieData.results[2].title}</h4>
-				  </div>
-			  <a href="<!--link to modal-->"><img src="${lastSearch.movieData.results[2].image.url}" alt="${lastSearch.movieData.results[2].title} poster style="width:100%""></a>
-		  </div>
-			  </div>
-	  </div>
-		  `
+	 //  for (var i = 0; i < 3; i++) {  
+		 var cnt=0;
+		 console.log(lastSearch);
+		 for(let i=(lastSearch);i>=0;i--){  //get recent history first
+			 //max 6 history
+			 if(cnt<7){
+				 var title=searchHistory[i].movieData.results[0].title;  //title from API
+				 var searchValue=searchHistory[i].title;   //saved search value in local storage
+				 var id = searchHistory[i].movieData.results[0].id.slice(7,16);
+				 if(searchHistory[i].movieData.results[0].hasOwnProperty('image')){
+					 var imgSrc=searchHistory[i].movieData.results[0].image.url;
+				 }
+				 else{
+					 console.log("don't have url");
+					 break;
+				 }
+				//  var img = $("<img>").attr({"src": imgSrc,"alt":title});
+				//  var h3=$("<h3>").text(title);
+	  const searchHistoryContent = `
+	   <div class="moviePoster-1 cell-small-4">
+		   <div class = "card">
+			   <div class = "card-section">
+				   <div class="centered-text">
+				   <h4>${title}</h4>
+				   </div>	
+				   <a href="<!--link to modal-->"><img src="${imgSrc}" alt="${title} poster style="width:100%""></a>
+		   </div>
+			   </div>
+	   </div>
+	   <div class="moviePoster-2 cell-small-4">
+		   <div class = "card">
+			   <div class = "card-section">
+				   <div class="centered-text">
+				   ${title}
+				   </div>	
+				   <a href="<!--link to modal-->"><img src="${imgSrc}" alt="${title} poster style="width:100%""></a>
+		   </div>
+			   </div>
+	   </div>
+	   <div class="moviePoster-3 cell-small-4">
+		   <div class = "card">
+			   <div class = "card-section">
+				   <div class="centered-text">
+				   ${title}
+				   </div>
+				   <a href="<!--link to modal-->"><img src="${imgSrc}" alt="${title} poster style="width:100%""></a>
+		   </div>
+			   </div>
+	   </div>
+		   `
 	moviePoster.innerHTML = searchHistoryContent;
 	  }
   }
+}
 init();
