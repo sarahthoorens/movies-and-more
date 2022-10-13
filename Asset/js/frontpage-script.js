87ff300d29f8f615fe91ab55a145acb9f15193bd
Sarah Thoorens <[s.thoorens@gmail.com]>
2022-10-13 11:29:33 -0500

    frontpage new js

--- Asset/js/frontpage-script.js
+++ Asset/js/frontpage-script.js
@@ -15,6 +15,7 @@ form.onsubmit = function (e) {
 	getMovieApi(movieInput);
 }
 	
+
 	// var moveUrlByID="https://online-movie-database.p.rapidapi.com/title/get-overview-details?tconst="+id+"&currentCountry=US";
     
     // const options = {
