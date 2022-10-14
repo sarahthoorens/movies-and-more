590d51e22e80f602c46431de5dcb43eaf1612700
Sarah Thoorens <[s.thoorens@gmail.com]>
2022-10-13 22:19:18 -0500

    changed youtube api key to fresh one in config folder

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = {
-  MY_YOUTUBE_API: "AIzaSyClJT_YIc2Vh0Aezue5FXzZ4X0K32N1SRo",
+  MY_YOUTUBE_API: "AIzaSyAbbSBc82WzmrxNATdjf-uLHKJUaDGGbSs", // Sarah's key 10-13-22
   MY_MOVIE_API : "c0bd93f36amshfaeaae0b910c85bp18a115jsn3771b8f37325",
   MY_GIPHY_API : "wvV8XbpunUwtfHCUuAUptsJJmdb4yn01",
 }
