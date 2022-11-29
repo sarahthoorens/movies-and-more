c6d9875d51fba48be481c7ba965e16798d82f566
Sarah Thoorens <106002158+sarahthoorens@users.noreply.github.com>
2022-11-29 12:34:13 -0600

    Update config.js
    giphy key change

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 var config = {
   MY_YOUTUBE_API: "AIzaSyAbbSBc82WzmrxNATdjf-uLHKJUaDGGbSs", // Sarah's key 10-13-22
   MY_MOVIE_API : "c0bd93f36amshfaeaae0b910c85bp18a115jsn3771b8f37325",
-  MY_GIPHY_API : "wvV8XbpunUwtfHCUuAUptsJJmdb4yn01",
+  MY_GIPHY_API :  "cPI8T7AcxZYQczXP8cvpV0y4zT3WgrOf",
 //sarah's keys:
-  myGiphyApi: 'cPI8T7AcxZYQczXP8cvpV0y4zT3WgrOf',
+  myGiphyApi:,
   myMovieApi: '0d66aff19dmsh524092da5aa051fp10c216jsn8b26997c98ec'
-
+"wvV8XbpunUwtfHCUuAUptsJJmdb4yn01"
 }
